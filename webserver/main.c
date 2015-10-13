3bc84f65b43cbff8ebbd8a76a3826f12492cd889
Mathias Quentin PICARD <mathias.picard@etudiant.univ-lille1.fr>
2015-10-13 19:05:19 +0200

    Probleme navigateur

--- webserver/main.c
+++ webserver/main.c
@@ -11,7 +11,7 @@
 int main(){
 	int taille=0;
 	int socket_client, pid, tokenCounter = 0, pageOK = 1;
-	char s[256];
+	char s[1024];
 	char str[1024];
 	FILE *f = NULL;
 	const char *message_bienvenue = "Bonjour, c'est un grand honneur de vous avoir sur notre serveur. Bienvenue à bord !\n";
@@ -33,35 +33,49 @@ int main(){
 				perror("Erreur write bienvenu");
 				traitement d’erreur 
 			}*/
-			
-			if(fgets(str, sizeof(str), f)) {
+			if(fgets(str, 1024/*sizeof(str)*/, f)) {
 				strcpy(s, str);
 				char* token = strtok(s, " ");
 				while (token) {
 					tokenCounter++;
-					if(!(tokenCounter == 1 && strcmp(token,"GET") == 0)){
-						printf("TOKEN 1\n");
+					if(tokenCounter == 1 && !strcmp(token,"GET") == 0){
 						pageOK = 0;
+					}
+					if(tokenCounter ==2 && !strcmp(token, "/") ==0){
+						pageOK=0;
+					}
+					if(tokenCounter == 3 && (strncmp(token, "HTTP/1.1",8) != 0 && strncmp(token, "HTTP/1.0",8) !=0)){
+						
+						pageOK = 0;
+					}
+					if(tokenCounter >=4){
+						printf("%s\n", token);
 					}
 					
-					if(!(tokenCounter == 3 && ( strcmp(token, "HTTP/1.1\r\n") == 0 || strcmp(token, "HTTP/1.0\r\n") == 0 ))){
-						printf("TOKEN 2\n");
-						pageOK = 0;
-					}
 					token = strtok(NULL, " ");
 				}
+				//	while(fgets(str, sizeof(str), f) != NULL) fprintf (stderr, "t");
+				fprintf(stderr,"z");
 				if(pageOK == 0){
+
 					fprintf(f, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
+					fflush(f);
+				}else{
+					//fprintf(stderr, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n %s \n\n", (int)strlen(message_bienvenue), message_bienvenue);
+					fprintf (stderr,"toto");
+					fprintf(f, "HTTP/1.1 200 OK\r\n");//Content-Length: %d\r\n\r\n %s \n\n", (int)strlen(message_bienvenue), message_bienvenue);
+					fflush(f);
+					fclose(f);
 				}
 			}
+			
+			
 			/*while((taille = read(socket_client, buffer, 256)) != 0){
 				if(write(socket_client, buffer, taille)== -1){
 					perror("Erreur write retour de message");
 				}
 			}*/
-			if(taille == -1){
-				perror("Erreur read retour");
-			}
+
 			exit(EXIT_SUCCESS);
 		}else{
 			close(socket_client);
